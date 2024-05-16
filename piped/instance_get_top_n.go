562170fbd555d26d41b7039d2690986a20dcfe72
Igor Cafe <igorcafe@protonmail.com>
2024-05-16 01:24:37 -0300

    wip: change how instance speed comparison is made

--- piped/instance_get_top_n.go
+++ piped/instance_get_top_n.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io"
 	"log"
+	"net/http"
 )
 
 func (s instanceService) GetTopN(ctx context.Context, n int, instances []Instance) ([]Instance, error) {
@@ -19,12 +20,38 @@ func (s instanceService) GetTopN(ctx context.Context, n int, instances []Instanc
 		inst := inst
 		piped := NewService(log.New(io.Discard, "", 0), inst.URL, s.http)
 		go func() {
-			_, err := piped.FindStreams(ctx, "mtaQroi75M0")
+			res, err := piped.FindStreams(ctx, "oRdxUFDoQe0")
 			if errors.Is(err, context.Canceled) {
 				return
 			}
 			if err != nil {
 				// log.Print("failed to interact with instance: ", err)
+				return
+			}
+
+			if len(res.VideoStreams) == 0 {
+				return
+			}
+
+			url := res.VideoStreams[len(res.VideoStreams)-1].URL
+
+			req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+			if err != nil {
+				return
+			}
+
+			resp, err := s.http.Do(req)
+			if err != nil {
+				return
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != 200 {
+				return
+			}
+
+			_, err = io.Copy(io.Discard, resp.Body)
+			if err != nil {
 				return
 			}
 
